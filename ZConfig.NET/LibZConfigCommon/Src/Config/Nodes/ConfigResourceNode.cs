2db82dd6a9a774736f43905749fe6580ed9b4717
Subho Ghosh <subho.ghosh@outlook.com>
2019-03-23 23:56:55 +0530

    Updated license headers.
    Sync commit.

--- ZConfig.NET/LibZConfigCommon/Src/Config/Nodes/ConfigResourceNode.cs
+++ ZConfig.NET/LibZConfigCommon/Src/Config/Nodes/ConfigResourceNode.cs
@@ -1,4 +1,30 @@
-﻿using System;
+﻿#region copyright
+//
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+//
+// Copyright (c) 2019
+// Date: 2019-3-23
+// Project: LibZConfigCommon
+// Subho Ghosh (subho dot ghosh at outlook.com)
+//
+//
+#endregion
+using System;
 using System.IO;
 using System.Collections.Generic;
 using System.Text;
@@ -23,9 +49,9 @@ namespace LibZConfig.Common.Config.Nodes
         /// </summary>
         DIRECTORY,
         /// <summary>
-        /// BLOB File Resource
+        /// Zip File Resource
         /// </summary>
-        BLOB
+        ZIP
     }
 
     /// <summary>
@@ -194,12 +220,17 @@ namespace LibZConfig.Common.Config.Nodes
     /// <summary>
     /// BLOB Resource Handle
     /// </summary>
-    public class ConfigResourceBlob : ConfigResourceFile
+    public class ConfigResourceZip : ConfigResourceFile
     {
+        /// <summary>
+        /// Local where the content is unzipped.
+        /// </summary>
+        public DirectoryInfo Directory { get; set; }
+
         /// <summary>
         /// Default Empty constructor
         /// </summary>
-        public ConfigResourceBlob() : base()
+        public ConfigResourceZip() : base()
         {
 
         }
@@ -209,7 +240,7 @@ namespace LibZConfig.Common.Config.Nodes
         /// </summary>
         /// <param name="configuration">Configuration instance</param>
         /// <param name="parent">Parent node.</param>
-        public ConfigResourceBlob(Configuration configuration, AbstractConfigNode parent) : base(configuration, parent)
+        public ConfigResourceZip(Configuration configuration, AbstractConfigNode parent) : base(configuration, parent)
         {
 
         }
--- ZConfig.NET/LibZConfigCommon/Src/Config/Parsers/XmlConfigParser.cs
+++ ZConfig.NET/LibZConfigCommon/Src/Config/Parsers/XmlConfigParser.cs
@@ -1,4 +1,30 @@
-﻿using System;
+﻿#region copyright
+//
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+//
+// Copyright (c) 2019
+// Date: 2019-3-23
+// Project: LibZConfigCommon
+// Subho Ghosh (subho dot ghosh at outlook.com)
+//
+//
+#endregion
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Xml;
@@ -392,8 +418,8 @@ namespace LibZConfig.Common.Config.Parsers
                 case EResourceType.DIRECTORY:
                     node = new ConfigDirectoryResource(configuration, parent);
                     break;
-                case EResourceType.BLOB:
-                    node = new ConfigResourceBlob(configuration, parent);
+                case EResourceType.ZIP:
+                    node = new ConfigResourceZip(configuration, parent);
                     break;
             }
             node.Name = elem.Name;
@@ -402,7 +428,7 @@ namespace LibZConfig.Common.Config.Parsers
             node.ResourceName = resourceName;
             if (settings.DownloadOptions == EDownloadOptions.LoadRemoteResourcesOnStartup)
             {
-                if (type == EResourceType.BLOB || type == EResourceType.FILE)
+                if (type == EResourceType.ZIP || type == EResourceType.FILE)
                 {
                     ConfigResourceFile fnode = (ConfigResourceFile)node;
                     using (AbstractReader reader = ReaderTypeHelper.GetReader(node.Location))
